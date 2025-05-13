cf5041f2609fe2ad9490598e9b6ba404f13386c0
ProKoKos <prokos@list.ru>
2025-05-13 17:03:30 +0300

    Шаг 6. Инициализация ролей и прав

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        \$perms = ['list users','edit users','delete users','list roles','edit roles'];
+        foreach (\$perms as \$p) {
+        Permission::firstOrCreate(['name' => \$p]);
+        }
+        \$admin = Role::firstOrCreate(['name' => 'admin']);
+        \$admin->syncPermissions(\$perms);
+        Role::firstOrCreate(['name' => 'user'])->givePermissionTo('list users');
+    }
+}
