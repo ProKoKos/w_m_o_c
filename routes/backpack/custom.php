5942e78ad403efa94e82ecf2235b8cceda42cc0f
ProKoKos <prokos@list.ru>
2025-05-13 16:07:56 +0300

    Шаг 4. Генерация CRUD-панелей для пользователей, ролей и прав

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -9,13 +9,21 @@ use Illuminate\Support\Facades\Route;
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-    'prefix' => config('backpack.base.route_prefix', 'admin'),
-    'middleware' => array_merge(
-        (array) config('backpack.base.web_middleware', 'web'),
-        (array) config('backpack.base.middleware_key', 'admin')
-    ),
+//    'prefix' => config('backpack.base.route_prefix', 'admin'),
+//    'middleware' => array_merge(
+//        (array) config('backpack.base.web_middleware', 'web'),
+//        (array) config('backpack.base.middleware_key', 'admin')
+//    ),
+    'prefix'     => config('backpack.base.route_prefix'),
+    'middleware' => ['web', 'admin'],
     'namespace' => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+//    Route::crud('user', 'UserCrudController');
+//    Route::crud('role', 'RoleCrudController');
+//    Route::crud('permission', 'PermissionCrudController');
+    Route::crud('user', 'UserCrudController');
+    Route::crud('role', 'RoleCrudController');
+    Route::crud('permission', 'PermissionCrudController');
 }); // this should be the absolute last line of this file
 
 /**
